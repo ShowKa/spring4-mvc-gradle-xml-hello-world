31ee40aa726ba2205613cb907b6ad57c93125a3d
kanamoto_shota <kanamoto.official@gmail.com>
2017-02-11 13:36:58 +0900

    Modify Domain Builder

--- src/main/java/com/mkyong/helloworld/domain/builder/AbstractDomainBuilder.java
+++ src/main/java/com/mkyong/helloworld/domain/builder/AbstractDomainBuilder.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mkyong.helloworld.domain.AbstractDomain;
-import com.sun.beans.decoder.ValueObject;
 
 /**
- * {@link ValueObject}のインスタンスを生成するビルダー。
+ * {@link AbstractDomain}のインスタンスを生成するビルダー。
  *
  * @param <T>
  *            ビルド対象のインスタンスの型
@@ -19,11 +18,11 @@ public abstract class AbstractDomainBuilder<T extends AbstractDomain, S extends 
 	List<BuilderConfigurator<S>> configurators = new ArrayList<BuilderConfigurator<S>>();
 
 	/**
-	 * ビルダの設定に基づき、引数の{@link ValueObject}の内容を変更した新しいインスタンスを生成する。
+	 * ビルダの設定に基づき、引数の{@link AbstractDomain}の内容を変更した新しいインスタンスを生成する。
 	 *
 	 * @param vo
-	 *            状態を引用する{@link ValueObject}
-	 * @return vo の内容に対して、このビルダの設定を上書きした{@link ValueObject}の新しいインスタンス
+	 *            状態を引用する{@link AbstractDomain}
+	 * @return vo の内容に対して、このビルダの設定を上書きした{@link AbstractDomain}の新しいインスタンス
 	 */
 	public T apply(T vo) {
 		S builder = newInstance();
@@ -35,9 +34,9 @@ public abstract class AbstractDomainBuilder<T extends AbstractDomain, S extends 
 	}
 
 	/**
-	 * ビルダの設定に基づいて{@link ValueObject}の新しいインスタンスを生成する。
+	 * ビルダの設定に基づいて{@link AbstractDomain}の新しいインスタンスを生成する。
 	 *
-	 * @return {@link ValueObject}の新しいインスタンス
+	 * @return {@link AbstractDomain}の新しいインスタンス
 	 */
 	public T build() {
 		for (BuilderConfigurator<S> configurator : configurators) {
@@ -58,26 +57,26 @@ public abstract class AbstractDomainBuilder<T extends AbstractDomain, S extends 
 	}
 
 	/**
-	 * 引数のビルダに対して、引数の{@link ValueObject}の内容を適用する。
+	 * 引数のビルダに対して、引数の{@link AbstractDomain}の内容を適用する。
 	 *
 	 * @param vo
-	 *            状態を引用する{@link ValueObject}
+	 *            状態を引用する{@link AbstractDomain}
 	 * @param builder
 	 *            ビルダ
 	 */
 	protected abstract void apply(T vo, S builder);
 
 	/**
-	 * ビルダの設定に基づいて{@link ValueObject}の新しいインスタンスを生成する。
+	 * ビルダの設定に基づいて{@link AbstractDomain}の新しいインスタンスを生成する。
 	 *
 	 * <p>
-	 * {@link ValueObjectBuilder#build}内でこのビルダに追加された{@link BuilderConfigurator}
-	 * を全て実行した後に、このメソッドが呼ばれる。<br>
-	 * その為、このビルダに対する変更を行うロジックはこのメソッド内に記述せず、目的となる{@link ValueObject}
+	 * {@link AbstractDomainBuilder#build}内でこのビルダに追加された
+	 * {@link BuilderConfigurator} を全て実行した後に、このメソッドが呼ばれる。<br>
+	 * その為、このビルダに対する変更を行うロジックはこのメソッド内に記述せず、目的となる{@link AbstractDomain}
 	 * を生成し返すロジックを記述することが望まれる。
 	 * </p>
 	 *
-	 * @return {@link ValueObject}の新しいインスタンス
+	 * @return {@link AbstractDomain}の新しいインスタンス
 	 */
 	protected abstract T createDomainObject();
 
@@ -96,7 +95,7 @@ public abstract class AbstractDomainBuilder<T extends AbstractDomain, S extends 
 	protected abstract S newInstance();
 
 	/**
-	 * {@link ValueObjectBuilder#build()}内で順次実行されるビルダの設定を定義するインタフェース。
+	 * {@link AbstractDomainBuilder#build()}内で順次実行されるビルダの設定を定義するインタフェース。
 	 *
 	 * @param <S>
 	 *            設定対象ビルダーの型
@@ -104,7 +103,7 @@ public abstract class AbstractDomainBuilder<T extends AbstractDomain, S extends 
 	public static interface BuilderConfigurator<S> {
 
 		/**
-		 * {@link ValueObjectBuilder#build()}内で呼ばれる際に実行するビルドアクションを定義する。
+		 * {@link AbstractDomainBuilder#build()}内で呼ばれる際に実行するビルドアクションを定義する。
 		 *
 		 * @param builder
 		 *            ビルダーインスタンス
--- src/main/java/com/mkyong/helloworld/domain/builder/KokyakuDomainBuilder.java
+++ src/main/java/com/mkyong/helloworld/domain/builder/KokyakuDomainBuilder.java
@@ -22,12 +22,18 @@ public class KokyakuDomainBuilder extends AbstractDomainBuilder<KokyakuDomain, K
 	/** 住所 */
 	private String address;
 
-	/** 主幹部署 */
+	/** 主幹部署コード */
 	private String shukanBushoCode;
 
+	// protected method
 	@Override
-	protected void apply(KokyakuDomain vo, KokyakuDomainBuilder builder) {
-		builder.withKokyakuCode(vo.getCode());
+	protected void apply(KokyakuDomain domain, KokyakuDomainBuilder builder) {
+		builder.withCode(domain.getCode());
+		builder.withGenteiKubun(domain.getGenteiKubun());
+		builder.withKokyakuKubun(domain.getKokyakuKubun());
+		builder.withName(domain.getName());
+		builder.withAddress(domain.getAddress());
+		builder.withShukanBushoCode(domain.getShukanBushoCode());
 	}
 
 	@Override
@@ -45,25 +51,26 @@ public class KokyakuDomainBuilder extends AbstractDomainBuilder<KokyakuDomain, K
 		return new KokyakuDomainBuilder();
 	}
 
+	// public method
 	/**
-	 * {@link KokyakuDomain}に与える名前をこのビルダに設定する。
+	 * {@link KokyakuDomain}に与える顧客コードをこのビルダに設定する。
 	 *
-	 * @param kokyakuCode
+	 * @param code
 	 *            顧客コード
 	 * @return {@link KokyakuDomainBuilder}
 	 */
-	public KokyakuDomainBuilder withKokyakuCode(final String kokyakuCode) {
+	public KokyakuDomainBuilder withCode(final String code) {
 		addConfigurator(new BuilderConfigurator<KokyakuDomainBuilder>() {
 			@Override
 			public void configure(KokyakuDomainBuilder builder) {
-				builder.code = kokyakuCode;
+				builder.code = code;
 			}
 		});
 		return getThis();
 	}
 
 	/**
-	 * {@link KokyakuDomain}に与える名前をこのビルダに設定する。
+	 * {@link KokyakuDomain}に与える限定区分をこのビルダに設定する。
 	 *
 	 * @param genteiKubun
 	 *            限定区分
@@ -80,10 +87,10 @@ public class KokyakuDomainBuilder extends AbstractDomainBuilder<KokyakuDomain, K
 	}
 
 	/**
-	 * {@link KokyakuDomain}に与える名前をこのビルダに設定する。
+	 * {@link KokyakuDomain}に与える顧客区分をこのビルダに設定する。
 	 *
-	 * @param genteiKubun
-	 *            限定区分
+	 * @param kokyakuKubun
+	 *            顧客区分
 	 * @return {@link KokyakuDomainBuilder}
 	 */
 	public KokyakuDomainBuilder withKokyakuKubun(final KokyakuKubun kokyakuKubun) {
@@ -91,6 +98,57 @@ public class KokyakuDomainBuilder extends AbstractDomainBuilder<KokyakuDomain, K
 			@Override
 			public void configure(KokyakuDomainBuilder builder) {
 				builder.kokyakuKubun = kokyakuKubun;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link KokyakuDomain}に与える顧客名をこのビルダに設定する。
+	 *
+	 * @param name
+	 *            顧客名
+	 * @return {@link KokyakuDomainBuilder}
+	 */
+	public KokyakuDomainBuilder withName(final String name) {
+		addConfigurator(new BuilderConfigurator<KokyakuDomainBuilder>() {
+			@Override
+			public void configure(KokyakuDomainBuilder builder) {
+				builder.name = name;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link KokyakuDomain}に与える住所をこのビルダに設定する。
+	 *
+	 * @param address
+	 *            住所
+	 * @return {@link KokyakuDomainBuilder}
+	 */
+	public KokyakuDomainBuilder withAddress(final String address) {
+		addConfigurator(new BuilderConfigurator<KokyakuDomainBuilder>() {
+			@Override
+			public void configure(KokyakuDomainBuilder builder) {
+				builder.address = address;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link KokyakuDomain}に与える主幹部署コードをこのビルダに設定する。
+	 *
+	 * @param shukanBushoCode
+	 *            主幹部署コード
+	 * @return {@link KokyakuDomainBuilder}
+	 */
+	public KokyakuDomainBuilder withShukanBushoCode(final String shukanBushoCode) {
+		addConfigurator(new BuilderConfigurator<KokyakuDomainBuilder>() {
+			@Override
+			public void configure(KokyakuDomainBuilder builder) {
+				builder.shukanBushoCode = shukanBushoCode;
 			}
 		});
 		return getThis();
